9db6199f308eafd010d0ac2d790e6430b2dec5dc
Aurelio Quezada Alvarez <jaquezadaa@gmail.com>
2018-03-18 20:36:17 -0600

    This commit includes a directory with the FactoryMethod files in C#

---
+++ FactoryMethod/ConcreteCreatorN.cs
@@ -0,0 +1,17 @@
+﻿//Author: José Aurelio Quezada Alvarez
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class ConcreteCreatorN : Creator
+    {
+        //A simple implementation of factory method
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductN();
+        }
+    }
+}
